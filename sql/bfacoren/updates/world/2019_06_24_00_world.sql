1723002626d53a6114a0b4915f354bbc43770f0a
cooler-SAI <cooler19860212@yahoo.com>
2019-06-24 15:14:44 +0300

    DB/Errors: more fixes

---
+++ sql/bfacoren/updates/world/2019_06_24_00_world.sql
@@ -0,0 +1,52 @@
+
+UPDATE `creature` SET `equipment_id`='1' WHERE `id` IN(SELECT `CreatureID` FROM `creature_equip_template` WHERE `ID`='1');
+
+UPDATE creature_template SET scriptname = 'npc_crystalline_scorpid' WHERE entry = 103217;
+
+
+
+UPDATE creature_template SET scriptname = 'npc_vha_demon_portal' WHERE entry = 92261;
+UPDATE creature_template SET scriptname = 'npc_vha_ice_block' WHERE entry = 102301;
+UPDATE creature_template SET scriptname = 'npc_vha_phase_spider' WHERE entry = 102434;
+UPDATE creature_template SET scriptname = 'npc_vha_mechanical_squirrel_bomb' WHERE entry = 102044;
+UPDATE creature_template SET scriptname = 'npc_vha_mechanical_squirrel_bomb' WHERE entry = 102084;
+UPDATE creature_template SET scriptname = 'npc_vha_mechanical_squirrel_bomb' WHERE entry = 102085;
+
+
+
+UPDATE creature_template SET scriptname = 'npc_faceless_tendril' WHERE entry = 101994;
+UPDATE creature_template SET scriptname = 'npc_vha_squirrel_bomb' WHERE entry = 102137;
+UPDATE creature_template SET scriptname = 'npc_vha_rocket_chicken' WHERE entry = 102139;
+UPDATE creature_template SET scriptname = 'npc_vha_congealing_goo' WHERE entry = 102158;
+UPDATE creature_template SET scriptname = 'npc_vha_stasis_crystal' WHERE entry = 103672;
+UPDATE creature_template SET scriptname = 'npc_vha_congealing_blood' WHERE entry = 102941;
+UPDATE creature_template SET scriptname = 'npc_vha_impale_dummy' WHERE entry = 999850;
+UPDATE creature_template SET scriptname = 'npc_vha_blistering_bettle' WHERE entry = 102540;
+UPDATE creature_template SET scriptname = 'npc_vha_spitting_scarab' WHERE entry = 102271;
+UPDATE creature_template SET scriptname = 'npc_vault_of_the_wardens_sledge_or_crusher' WHERE entry = 197180;
+
+UPDATE `creature` SET `ScriptName`='npc_altruis_sufferer_4' WHERE `guid`=20542908;
+
+UPDATE creature_template SET scriptname = 'npc_maiev_shadowsong' WHERE entry = 92718;
+UPDATE creature_template SET scriptname = 'npc_mannethrel' WHERE entry = 96652;
+UPDATE creature_template SET scriptname = 'npc_korvas' WHERE entry = 99045;
+UPDATE creature_template SET scriptname = 'npc_cyana' WHERE entry = 96420;
+UPDATE creature_template SET scriptname = 'npc_allari' WHERE entry = 96655;
+
+UPDATE creature_template SET scriptname = 'npc_kayn_sunfury' WHERE entry = 93127;
+UPDATE creature_template SET scriptname = 'npc_brood_queen_tyranna' WHERE entry = 93802;
+UPDATE creature SET scriptname = 'npc_korvas_tyranna_fight' WHERE guid = 20542498;
+UPDATE creature SET scriptname = 'npc_allari_tyranna_fight' WHERE guid = 20542497;
+UPDATE creature SET scriptname = 'npc_kayn_tyranna_fight' WHERE guid = 20542609;
+UPDATE creature_template SET scriptname = 'npc_putrid_sludge' WHERE entry = 113512;
+UPDATE creature_template SET scriptname = 'npc_tr_slime_pool' WHERE entry = 112293;
+UPDATE creature_template SET scriptname = 'npc_sludgerax' WHERE entry = 112255;
+UPDATE creature_template SET scriptname = 'npc_arc_vicious_manafang' WHERE entry = 110966;
+UPDATE creature_template SET scriptname = 'npc_zaela_chapter1' WHERE entry = 43190;
+
+UPDATE creature_template SET scriptname = 'npc_crystalline_shard' WHERE entry = 103209;
+UPDATE creature_template SET scriptname = 'npc_arcane_tether' WHERE entry = 103682;
+UPDATE creature_template SET scriptname = 'npc_temporal_rift' WHERE entry = 106878;
+UPDATE creature_template SET scriptname = 'npc_fragmented_time_particle' WHERE entry = 114671;
+UPDATE creature_template SET scriptname = 'npc_waning_time_particle' WHERE entry = 104676;
+
